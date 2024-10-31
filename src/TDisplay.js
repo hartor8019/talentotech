2fc416e1d59d546e3d5a21337bfcf3476556d6b0
Harold Torres <harold.torres425@gmail.com>
2024-10-31 15:39:56 -0500

    Modificacion de TDisplay.js

--- src/TDisplay.js
+++ src/TDisplay.js
@@ -4,7 +4,6 @@ import React from 'react';
 const Display = ({ input, result }) => (
   <div className="display">
     <div className="input">{input}</div>
-    <div className="result">{result}</div>
   </div>
 );
 
